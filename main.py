56b1583e661221d5f25c30135bd40142cc093e09
kahbyte <kaue.sbsousa@gmail.com>
2022-06-05 21:10:40 -0300

    added classes command

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
+from statistics import mode
 import discord
 import os
 from dotenv import load_dotenv
-#import trained_model as model
+import trained_model as model
 
 
 # --------- Modelo --------
 #Passagem da url da imagem para o modelo analizar
 
-#model = model.Model()
-#loaded_model = model.load_model()
+model = model.Model()
+loaded_model = model.load_model()
 
 load_dotenv()
 
@@ -42,57 +43,71 @@ async def on_message(message):
 
   #comando com marcador $ para reconhecer um comando
   #if message.content.startswith('$'):
-  if(content == prefix + "iniciar"):
-    #await message.delete()
-    await channel.send("Bot iniciado!")
-    
-  #if (anexo and (content == prefix + "emp")):
-   
 
-  if (content == prefix + "emp"):
-    if (anexo):
-      url = anexo[0].proxy_url
-      #await message.delete()
-      print(url)
+  if (content.startswith(prefix)):
+    if(content == prefix + "iniciar"):
+      await channel.send("Bot iniciado!")
+      return
+    
+    elif content.startswith(prefix + "emp"):
+      print("recebi emp")
+      if (anexo):
+        print("imagem anexada")
+        url = anexo[0].proxy_url
+        print(url)
+        
+      else:
+        print("imagem com link")
+        img = content.split()
+        url = img[1]
+        print(img)
+        print(url)
+      response = model.predict(loaded_model, url)
+      await channel.send("A sua placa é: `" + response + "`")
+      return
       
-    else:
-      img = content.split()
-      url = img[1]
-      print(img)
-      print(url)
+    elif (content == prefix + "sobre"):
+      sobre = """
+      Projeto Integrador 5 - Aplicação de Inteligência Artificial
+
+      Alunos:
+        * Gabriel Kenji
+        * Kauê Sales
+        * Thiago Felix
+        * Vinycius Zanardi
+
+      Web-site:
+      Artigo Científico: 
+      Para mais informações: https://github.com/kahbyte/Emplacado 
+      """
+      await channel.send(sobre)
     
+    elif (content == prefix + "site"):
+      await message.delete()
+      await channel.send("regra do senac é seu c*")
+    
+    elif (content == prefix + "classes"):
+      classes = model.classes.items()
 
-  if (content == prefix + "sobre"):
-    sobre = """
-    Projeto Integrador 5 - Aplicação de Inteligência Artificial
+      list = """"""
 
-    Alunos:
-      * Gabriel Kenji
-      * Kauê Sales
-      * Thiago Felix
-      * Vinycius Zanardi
+      for key, value in classes:
+        list += (f"{key}: {value}") + "\n"
+      print(list) 
 
-    Web-site:
-    Artigo Científico: 
-    Para mais informações: https://github.com/kahbyte/Emplacado 
-    """
-    await channel.send(sobre)
+      await channel.send("`" + list + "`")
 
-
-  if (content == prefix + "help"):
-    help = """Comandos:
-    $emp - para reconhecer uma placa
-    $sobre - para saber mais referente ao projeto
-    """
-    await channel.send(help)
-     
-  if (content == prefix + "site"):
-    await message.delete()
-    await channel.send("regra do senac é seu c*")
-
-  #response = model.predict(loaded_model, url)
-  #await channel.send("A sua placa é: `" + response + "`")
-
+    elif (content == prefix + "help"):
+      help = """Comandos:
+      $emp - e adicione uma imagem via link ou anexo para reconhecer uma placa
+      $sobre - para mais informações referentes ao projeto
+      $classes - para conhecer as placas que podem ser reconhecidas
+      """
+      await channel.send(help)
+    
+    else:
+      error = "comando invalido. Digite $help."
+      await channel.send(error)
 
 #Executar e atualizar o Bot
 client.run(TOKEN)
